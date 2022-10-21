140c46d275d70d10996ceaef5440a5eea165f007
TRANCASAIT <soporteit@trancasa.com>
2022-10-21 10:06:57 -0500

    HttpClientModule Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -55,7 +55,8 @@ import { UsuariosComponent } from './components/usuarios/usuarios.component';
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
